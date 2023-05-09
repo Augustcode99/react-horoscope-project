f89abf0ac607ceed0c5779d81fc62625bfa7bbe7
Augustcode99 <burakodabasi99@yahoo.com>
2023-05-09 14:26:13 +0300

    cards displayed

--- src/App.js
+++ src/App.js
@@ -2,14 +2,27 @@ import "./scss/app.scss";
 import Navbar from "./components/Navbar.jsx";
 import Header from "./components/Header.jsx";
 import Card from "./components/Card.jsx";
-import data from "./helpers/data";
+import data from "./helpers/data.js";
 
 function App() {
   return (
     <div>
       <Navbar />
       <Header />
-      <Card />
+      <div className="cardContainer">
+        {data.map((item) => {
+          const { id, title, date, image, desc } = item;
+          return (
+            <Card
+              key={id}
+              title={title}
+              image={image}
+              desc={desc}
+              date={date}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,15 @@
 import cardStyle from "../scss/card.module.scss";
 
-const Card = () => {
-  return <div className={cardStyle.card}>Card</div>;
+const Card = (props) => {
+  const { title, date, desc, image } = props;
+  return (
+    <div className={cardStyle.card}>
+      <h1>{title}</h1>
+      <h3>{date}</h3>
+      <img src={image} alt={title} />
+      <p>{desc}</p>
+    </div>
+  );
 };
 
 export default Card;
